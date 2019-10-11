1779519e7a886a41070a871b96a08563220bd065
Rafael Francisco <rafaelfrancisco6@gmail.com>
2019-10-11 11:20:00 +0100

    Added minimium platform version

--- Package.swift
+++ Package.swift
@@ -1,9 +1,12 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "ARCoreLocation",
+    platforms: [
+        .iOS(.v11)
+    ],
     products: [
         .library(name: "ARCoreLocation",  targets: ["ARCoreLocation"])
     ],
